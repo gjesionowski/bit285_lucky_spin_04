a06c58dd06a15715110563fcbcc2a9dd6b201482
Gregory Jesionowski <gregory.a.jesionowski@gmail.com>
2020-01-21 14:41:07 -0800

    Updating repository

---
+++ LuckySpin/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class Player
+    {
+        string name = "";
+        int luckyNum;
+    }
+}
