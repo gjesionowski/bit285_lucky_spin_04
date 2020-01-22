9a7a7f21438dad16b8ae951a8bc844ef660f9a35
Gregory Jesionowski <gregory.a.jesionowski@gmail.com>
2020-01-21 20:18:33 -0800

    Update checkpoint

---
+++ LuckySpin/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class Player
+    {
+        public int luckyNum { get; set; }
+        public string playerName { get; set; }
+    }
+}
